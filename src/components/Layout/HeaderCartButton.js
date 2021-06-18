550a564fd2cad14afdbcc0ee6ec5f57a747311bf
Jeroen Verhoeven <verhoevemans@gmail.com>
2021-06-18 21:26:01 +0200

    Update cart to add and remove items, add CartItem component, add animation in Header button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
+
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
+  
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+    
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
 
   const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
-
+  
+  const buttonClasses = `${classes.button} ${buttonIsHighlighted ? classes.bump : ''}`;
+  
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
