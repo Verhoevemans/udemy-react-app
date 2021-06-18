cf608af26d458e462020a65418f91d0f9f6535ff
Verhoeven, J. (Jeroen) <Jeroen.Verhoeven@devolksbank.nl>
2021-06-18 15:32:01 +0200

    Add cart component and modal, use props to control modal, add and use context for cart content

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,29 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = +amountInputRef.current.value;
+
+    if (!enteredAmount || enteredAmount < 1 || enteredAmount > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmount);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input label="amount"
+             ref={amountInputRef}
              input={{
                id: 'amount_' + props.id,
                type: 'number',
@@ -15,6 +33,7 @@ const MealItemForm = (props) => {
                defaultValue: 1
              }}/>
       <button type="submit">+ Add</button>
+      {!amountIsValid && <p><i>Please enter a valid amount (1-5).</i></p>}
     </form>
   );
 };
